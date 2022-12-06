ed84a29a4595d18c2cfb89d9e16c7ba5dec3e5c0
Lorin-Lange <lo.-lange@arcor.de>
2022-12-06 10:12:24 +0100

    added 6th solution

--- AoC_06/AoC06.hs
+++ AoC_06/AoC06.hs
@@ -12,18 +12,16 @@ getInput :: IO String
 getInput = readFile "./input.txt"
 
 checkString :: String -> Int -> Int -> Int
-checkString str i l =
-    let isValid = isDifferent $ checkCharacters str range
-        range   = [0+i..l+i-1]
-    in if isValid then maximum range + 1 else checkString str (i+1) l
-    where checkCharacters str = map (str !!)
+checkString str i len =
+    let isValid = isDifferent $ map (str !!) range
+        range   = [0+i..len+i-1]
+    in if isValid then maximum range + 1 else checkString str (i+1) len
 
 isDifferent :: String -> Bool
-isDifferent str = 
+isDifferent string = 
     all (\((_, c1), (_, c2)) -> c1 /= c2) $ 
-    filter (\((i1, _), (i2, _)) -> i1 /= i2) $ isDifferentH str
-    where isDifferentH :: String -> [((Int, Char), (Int, Char))]
-          isDifferentH str = do let s = zip [0..] str
+    filter (\((i1, _), (i2, _)) -> i1 /= i2) $ isDifferentH string
+    where isDifferentH str = do let s = zip [0..] str
                                 c1 <- s
                                 c2 <- s
                                 return (c1, c2)
