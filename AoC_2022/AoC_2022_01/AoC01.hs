579974897060a4bfc81bdcba75957a60accb680c
Lorin-Lange <lo.-lange@arcor.de>
2024-06-22 18:10:46 +0200

    updated AoC01.hs

--- AoC_2022/AoC_2022_01/AoC01.hs
+++ AoC_2022/AoC_2022_01/AoC01.hs
@@ -15,12 +15,12 @@ getInput :: IO [Maybe Integer]
 getInput = do lst <- lines <$> readFile "input.txt"
               return $ map readMaybe lst
 
-sum' :: [Maybe Integer] -> [Integer]
-sum' lst = reverse . sort . map (sum . map (fromMaybe 0)) $ endBy [Nothing] lst
+getSums :: [Maybe Integer] -> [Integer]
+getSums = reverse . sort . map (sum . map (fromMaybe 0)) . endBy [Nothing]
 
 main :: IO()
 main = do
     lst <- getInput
-    let sums = sum' lst
+    let sums = getSums lst
     putStrLn $ "Part 1: " ++ show (head sums)
     putStrLn $ "Part 2: " ++ show (sum $ take 3 sums)
