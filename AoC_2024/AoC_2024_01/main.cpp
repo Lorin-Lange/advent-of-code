995c52f0d2cc1a62588b2f68cda88868738534ca
Lorin-Lange <lo.-lange@arcor.de>
2024-06-22 16:41:25 +0200

    added sample file

---
+++ AoC_2024/AoC_2024_01/main.cpp
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////
+//              Advent of Code 2022               //
+//           Day 2: Rock Paper Scissors           //
+//            Solution by Lorin Lange             //
+////////////////////////////////////////////////////
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return EXIT_SUCCESS;
+}
