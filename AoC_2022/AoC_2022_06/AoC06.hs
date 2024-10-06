abbf1d4e22d4a8a86c5156a44ac6180ba873891c
Lorin-Lange <lo.-lange@arcor.de>
2024-10-06 22:25:34 +0200

    refactored the Haskell solution of day 6 of year 2022

--- AoC_2022/AoC_2022_06/AoC06.hs
+++ AoC_2022/AoC_2022_06/AoC06.hs
@@ -1,15 +1,10 @@
 ----------------------------------------------------
---                                                --
 --              Advent of Code 2022               --
 --             Day 6: Tuning Trouble              --
 --            Solution by Lorin Lange             --
---                                                --
 ----------------------------------------------------
 
 module AoC06 where
-
-getInput :: IO String
-getInput = readFile "./input.txt"
 
 checkString :: String -> Int -> Int -> Int
 checkString str i len =
@@ -18,18 +13,14 @@ checkString str i len =
     in if isValid then maximum range + 1 else checkString str (i+1) len
 
 isDifferent :: String -> Bool
-isDifferent string = 
-    all (\((_, c1), (_, c2)) -> c1 /= c2) $ 
-    filter (\((i1, _), (i2, _)) -> i1 /= i2) $ isDifferentH string
-    where isDifferentH str = do let s = zip [0..] str
-                                c1 <- s
-                                c2 <- s
-                                return (c1, c2)
+isDifferent = all (\((_, c1), (_, c2)) -> c1 /= c2) 
+            . filter (\((i1, _), (i2, _)) -> i1 /= i2)
+            . \s -> zip [0..] s >>= 
+                \c1 -> zip [0..] s >>= 
+                    \c2 -> return (c1, c2)
 
 main :: IO()
 main = do
-    input <- getInput
-    print "Result of part one"
-    print $ checkString input 0 4
-    print "Result of part two"
-    print $ checkString input 0 14
+    input <- readFile "./input.txt"
+    putStrLn $ "Result of part one: " ++ show (checkString input 0 4)
+    putStrLn $ "Result of part two: " ++ show (checkString input 0 14)
