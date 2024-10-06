b6340a66f9fc5559a344accd0c049589908a6d38
Lorin-Lange <lo.-lange@arcor.de>
2024-10-06 21:14:17 +0200

    improved namings in the Haskell solution of day 25 of 2022

--- AoC_2022/AoC_2022_25/AoC25.hs
+++ AoC_2022/AoC_2022_25/AoC25.hs
@@ -7,21 +7,21 @@
 module AoC25 where
 
 toDecimal :: [Integer] -> Integer
-toDecimal snafu = sum $
-  zipWith (\s i -> s * 5^i) (reverse snafu) [0..]
+toDecimal encoded = sum $
+  zipWith (\s i -> s * 5^i) (reverse encoded) [0..]
 
 parse :: String -> [Integer]
 parse = map c where 
   c '=' = -2; c '-' = -1; c '0' = 0; c '1' = 1; c '2' = 2
 
-decimalTo :: Integer -> String
-decimalTo 0 = []
-decimalTo i = let (i', r) = (i + 2) `divMod` 5 
-              in decimalTo i' ++ [c r]
+toSNAFU :: Integer -> String
+toSNAFU 0 = []
+toSNAFU i = let (i', r) = (i + 2) `divMod` 5 
+            in toSNAFU i' ++ [c r]
   where c 0 = '='; c 1 = '-'; c 2 = '0'; c 3 = '1'; c 4 = '2'
 
 main :: IO ()
 main = do
     input <- lines <$> readFile "./input.txt"
-    let res = decimalTo . sum . map (toDecimal . parse) $ input
+    let res = toSNAFU . sum . map (toDecimal . parse) $ input
     putStrLn $ "Result: " ++ res
