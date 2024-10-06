10fb7c04ffc2394d0955efa7620a6ef37a459dc2
Lorin-Lange <lo.-lange@arcor.de>
2024-10-06 20:59:58 +0200

    simplified Haskell solution of day 25 of year 2022

--- AoC_2022/AoC_2022_25/AoC25.hs
+++ AoC_2022/AoC_2022_25/AoC25.hs
@@ -6,55 +6,22 @@
 
 module AoC25 where
 
-getInput :: IO [String]
-getInput = lines <$> readFile "./input.txt"
+toDecimal :: [Integer] -> Integer
+toDecimal snafu = sum $
+  zipWith (\s i -> s * 5^i) (reverse snafu) [0..]
 
-data SNAFUSymbol = 
-  SDMinus | SMinus | SNAFU0 | SNAFU1 | SNAFU2
+parse :: String -> [Integer]
+parse = map c where 
+  c '=' = -2; c '-' = -1; c '0' = 0; c '1' = 1; c '2' = 2
 
-type SNAFU = [SNAFUSymbol]
-
-snafuToDecimal :: SNAFU -> Integer
-snafuToDecimal snafu = 
-    let ufans = reverse snafu 
-    in sum $ zipWith (curry convert) ufans [0..]
-    where convert (SDMinus,i) = -2 * 5^i
-          convert (SMinus, i) = -1 * 5^i
-          convert (SNAFU0, i) =  0 * 5^i
-          convert (SNAFU1, i) =  1 * 5^i
-          convert (SNAFU2, i) =  2 * 5^i
-
-snafuToString :: SNAFU -> String
-snafuToString []          = ""
-snafuToString (SDMinus:r) = '=' : snafuToString r
-snafuToString (SMinus :r) = '-' : snafuToString r
-snafuToString (SNAFU0 :r) = '0' : snafuToString r
-snafuToString (SNAFU1 :r) = '1' : snafuToString r
-snafuToString (SNAFU2 :r) = '2' : snafuToString r
-
-decimalToSNAFU :: Integer -> SNAFU
-decimalToSNAFU 0 = []
-decimalToSNAFU i = 
-  let (i', r) = (i + 2) `divMod` 5 
-  in decimalToSNAFU i' ++ [c r]
-    where c 0 = SDMinus
-          c 1 = SMinus
-          c 2 = SNAFU0
-          c 3 = SNAFU1
-          c 4 = SNAFU2
-
-parseSNAFU :: String -> SNAFU
-parseSNAFU []      = [] 
-parseSNAFU ('=':r) = SDMinus : parseSNAFU r
-parseSNAFU ('-':r) = SMinus  : parseSNAFU r
-parseSNAFU ('0':r) = SNAFU0  : parseSNAFU r
-parseSNAFU ('1':r) = SNAFU1  : parseSNAFU r
-parseSNAFU ('2':r) = SNAFU2  : parseSNAFU r
+decimalTo :: Integer -> String
+decimalTo 0 = []
+decimalTo i = let (i', r) = (i + 2) `divMod` 5 
+              in decimalTo i' ++ [c r]
+  where c 0 = '='; c 1 = '-'; c 2 = '0'; c 3 = '1'; c 4 = '2'
 
 main :: IO ()
 main = do
-    input <- getInput
-    print $ sum . map (snafuToDecimal . parseSNAFU) $ input
-    print "Result of part one"
-    print . snafuToString . decimalToSNAFU . 
-      sum . map (snafuToDecimal . parseSNAFU) $ input
+    input <- lines <$> readFile "./input.txt"
+    let res = decimalTo . sum . map (toDecimal . parse) $ input
+    putStrLn $ "Result: " ++ res
