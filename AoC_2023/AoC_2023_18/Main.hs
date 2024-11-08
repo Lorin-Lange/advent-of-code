3a18798cb57ad788c8d98f95d9709215b3f1ccc8
Lorin-Lange <lo.-lange@arcor.de>
2024-11-08 23:52:49 +0100

    updated solution of day 18 of AoC '23

--- AoC_2023/AoC_2023_18/Main.hs
+++ AoC_2023/AoC_2023_18/Main.hs
@@ -13,11 +13,9 @@ getCoordinates :: [(Char, Int)] -> Coordinate -> [Coordinate]
 getCoordinates [] coord = [coord]
 getCoordinates ((c, n):xs) (x, y) = 
     (x, y) : getCoordinates xs (x + dx * n, y + dy * n)
-    where (dx, dy) = coordinate c
-          coordinate 'U' = ( 0, -1)
-          coordinate 'D' = ( 0,  1)
-          coordinate 'L' = (-1,  0)
-          coordinate 'R' = ( 1,  0)
+    where (dx, dy) = co c
+          co 'U' = ( 0, -1); co 'D' = (0, 1)
+          co 'L' = (-1,  0); co 'R' = (1, 0)
 
 determinant :: Num a => (a, a) -> (a, a) -> a
 determinant (x1, y1) (x2, y2) = x1 * y2 - x2 * y1
@@ -37,28 +35,24 @@ parseInput1 :: String -> (Char, Int)
 parseInput1 (c:_:xs) = (c, read . head $ splitOn " " xs)
 
 parseInput2 :: String -> (Char, Int)
-parseInput2 (c:_:xs) = let n        = init $ splitOn "#" xs !! 1
-                           [(i, _)] = readHex $ init n
-                       in (convertChar $ last n, i)
-                       where convertChar '0' = 'R'
-                             convertChar '1' = 'D'
-                             convertChar '2' = 'L'
-                             convertChar '3' = 'U'
+parseInput2 (c:_:xs) = (cc $ last n, i)
+    where [(i, _)] = readHex $ init n
+          n = init $ splitOn "#" xs !! 1
+          cc '2' = 'L'; cc '0' = 'R'
+          cc '3' = 'U'; cc '1' = 'D'
 
 main :: IO()
 main = do
     inp <- lines <$> readFile "input.txt"
 
     let lst1      = map parseInput1 inp
-    let coords1   = getCoordinates lst1 (0, 0)
-    let area1     = shoelaceFormula coords1
+    let area1     = shoelaceFormula (getCoordinates lst1 (0, 0))
     let boundary1 = fromIntegral $ sum $ map snd lst1
     let res1      = floor $ picksTheorem area1 boundary1
     putStrLn $ "Part 1: " ++ show res1
 
     let lst2      = map parseInput2 inp
-    let coords2   = getCoordinates lst2 (0, 0)
-    let area2     = shoelaceFormula coords2
+    let area2     = shoelaceFormula (getCoordinates lst2 (0, 0))
     let boundary2 = fromIntegral $ sum $ map snd lst2
     let res2      = floor $ picksTheorem area2 boundary2
     putStrLn $ "Part 2: " ++ show res2
