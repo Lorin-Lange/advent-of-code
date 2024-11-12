d8e962b3681224a674c464e0a02b40f6ec1791b8
Lorin-Lange <lo.-lange@arcor.de>
2024-11-12 16:11:25 +0100

    updated solution of day 1 of AoC '22

--- AoC_2022/AoC_2022_01/Main.hs
+++ AoC_2022/AoC_2022_01/Main.hs
@@ -12,13 +12,10 @@ import Data.List.Split ( endBy )
 import Data.Maybe ( fromMaybe )
 import Data.Ord ( Down(Down), comparing )
 
-getInput :: IO [Maybe Integer]
-getInput = map readMaybe . lines <$> readFile "input.txt"
-
 getSums :: [Maybe Integer] -> [Integer]
 getSums = sortBy (comparing Down) . map (sum . map (fromMaybe 0)) . endBy [Nothing]
 
 main :: IO()
-main = do sums <- getSums <$> getInput
+main = do sums <- getSums . map readMaybe . lines <$> readFile "input.txt"
           putStrLn $ "Part 1: " ++ show (head sums)
           putStrLn $ "Part 2: " ++ show (sum $ take 3 sums)
