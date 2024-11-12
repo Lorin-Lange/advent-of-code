c269f61f7b88509cc268a34224557d099b010e96
Lorin-Lange <lo.-lange@arcor.de>
2024-11-12 04:25:16 +0100

    refactored solution of day 25 of AoC '22

--- AoC_2022/AoC_2022_25/AoC25.hs
+++ AoC_2022/AoC_2022_25/AoC25.hs
@@ -9,14 +9,14 @@ module AoC25 where
 toDecimal :: [Integer] -> Integer
 toDecimal encoded = sum $ zipWith (\s i -> s * 5^i) (reverse encoded) [0..]
 
-parse :: String -> [Integer]
-parse = map c where c '=' = -2; c '-' = -1; c '0' = 0; c '1' = 1; c '2' = 2
-
 toSNAFU :: Integer -> String
 toSNAFU 0 = ""
 toSNAFU i = toSNAFU i' ++ [c r]
   where (i', r) = (i + 2) `divMod` 5
         c 0 = '='; c 1 = '-'; c 2 = '0'; c 3 = '1'; c 4 = '2'
+
+parse :: String -> [Integer]
+parse = map c where c '=' = -2; c '-' = -1; c '0' = 0; c '1' = 1; c '2' = 2
 
 main :: IO ()
 main = do input <- lines <$> readFile "./input.txt"
