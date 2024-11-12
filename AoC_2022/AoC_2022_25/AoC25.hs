36475f8584d952450c42f818c22034c108b194f3
Lorin-Lange <lo.-lange@arcor.de>
2024-11-12 04:24:14 +0100

    refactored solution of day 25 of AoC '22

--- AoC_2022/AoC_2022_25/AoC25.hs
+++ AoC_2022/AoC_2022_25/AoC25.hs
@@ -7,12 +7,10 @@
 module AoC25 where
 
 toDecimal :: [Integer] -> Integer
-toDecimal encoded = sum $
-  zipWith (\s i -> s * 5^i) (reverse encoded) [0..]
+toDecimal encoded = sum $ zipWith (\s i -> s * 5^i) (reverse encoded) [0..]
 
 parse :: String -> [Integer]
-parse = map c where 
-  c '=' = -2; c '-' = -1; c '0' = 0; c '1' = 1; c '2' = 2
+parse = map c where c '=' = -2; c '-' = -1; c '0' = 0; c '1' = 1; c '2' = 2
 
 toSNAFU :: Integer -> String
 toSNAFU 0 = ""
@@ -21,7 +19,6 @@ toSNAFU i = toSNAFU i' ++ [c r]
         c 0 = '='; c 1 = '-'; c 2 = '0'; c 3 = '1'; c 4 = '2'
 
 main :: IO ()
-main = do
-    input <- lines <$> readFile "./input.txt"
-    let res = toSNAFU . sum . map (toDecimal . parse) $ input
-    putStrLn $ "Result: " ++ res
+main = do input <- lines <$> readFile "./input.txt"
+          let res = toSNAFU . sum . map (toDecimal . parse) $ input
+          putStrLn $ "Result: " ++ res
