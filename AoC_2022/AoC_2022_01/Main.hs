05c6a0412b3f89ef9e668b9931b00b14efdd44a2
Lorin-Lange <lo.-lange@arcor.de>
2024-11-12 16:07:25 +0100

    updated solution of day 1 of AoC '22

--- AoC_2022/AoC_2022_01/Main.hs
+++ AoC_2022/AoC_2022_01/Main.hs
@@ -4,23 +4,21 @@
 --            Solution by Lorin Lange             --
 ----------------------------------------------------
 
-module AoC01 where
+module Main where
 
 import Text.Read ( readMaybe )
 import Data.List ( sortBy )
 import Data.List.Split ( endBy )
 import Data.Maybe ( fromMaybe )
-import Data.Ord ( Down(Down), comparing ) 
+import Data.Ord ( Down(Down), comparing )
 
 getInput :: IO [Maybe Integer]
-getInput = do lst <- lines <$> readFile "input.txt"
-              return $ map readMaybe lst
+getInput = map readMaybe . lines <$> readFile "input.txt"
 
 getSums :: [Maybe Integer] -> [Integer]
 getSums = sortBy (comparing Down) . map (sum . map (fromMaybe 0)) . endBy [Nothing]
 
 main :: IO()
-main = do
-    sums <- getSums <$> getInput
-    putStrLn $ "Part 1: " ++ show (head sums)         --  68442
-    putStrLn $ "Part 2: " ++ show (sum $ take 3 sums) -- 204837
+main = do sums <- getSums <$> getInput
+          putStrLn $ "Part 1: " ++ show (head sums)
+          putStrLn $ "Part 2: " ++ show (sum $ take 3 sums)
