cff8ca8f31111bb2468cb2917255d7041bfb8d53
Lorin-Lange <lo.-lange@arcor.de>
2024-11-15 23:29:49 +0100

    added dirty solution of day 14 of AoC '22

--- AoC_2022/AoC_2022_14/Main.hs
+++ AoC_2022/AoC_2022_14/Main.hs
@@ -1,3 +1,8 @@
+----------------------------------------------------
+--              Advent of Code 2022               --
+--           Day 14: Regolith Reservoir           --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
 
 {-# LANGUAGE TupleSections #-}
 
@@ -6,7 +11,7 @@ module Main where
 import Data.List.Split ( endBy, splitOn )
 import qualified Data.Map as Map
 
-data Material = Rock | Sand
+data Material = Rock | Sand | Floor
     deriving (Show, Eq)
 
 type Grid = Map.Map (Int, Int) Material
@@ -22,28 +27,48 @@ getPoints ((c1,r1):(c2,r2):xs)
     | otherwise = error "Error while expanding points"
     -- = case!?
 
-
 check :: Int -> (Int, Int) -> Grid -> Int -> Int
 check max k@(c, r) g counter
     | r > max = counter
     | otherwise =
     case Map.lookup (c, r+1) g of
-        (Just m) -> case Map.lookup (c-1, r+1) g of
-                        (Just m') -> case Map.lookup (c+1, r+1) g of
-                                        (Just m'') -> check max (500,-1) (Map.insert k Sand g) (counter + 1)
+        (Just _) -> case Map.lookup (c-1, r+1) g of
+                        (Just _) -> case Map.lookup (c+1, r+1) g of
+                                        (Just _) -> check max (500,-1) (Map.insert k Sand g) (counter + 1)
                                         Nothing    -> check max (c+1, r+1) g counter
-                        Nothing   -> check max (c-1, r+1) g counter
+                        Nothing  -> check max (c-1, r+1) g counter
         Nothing  -> check max (c, r+1) g counter
 
+check2 :: (Int, Int) -> Grid -> Int -> Int
+check2 k@(c, r) g counter =
+    case Map.lookup (500, 0) g of
+        (Just _) -> counter
+        Nothing  ->
+                    case Map.lookup (c, r+1) g of
+                        (Just _) -> case Map.lookup (c-1, r+1) g of
+                                        (Just _) -> case Map.lookup (c+1, r+1) g of
+                                                        (Just _) -> check2 (500,-1) (Map.insert k Sand g) (counter + 1)
+                                                        Nothing    -> check2 (c+1, r+1) g counter
+                                        Nothing  -> check2 (c-1, r+1) g counter
+                        Nothing  -> check2 (c, r+1) g counter
+
+
+
+
+
 parse :: String -> [(Int, Int)]
-parse = map (\st ->
-    (read $ head $ splitOn "," st, read $ splitOn "," st !! 1))
-    . endBy " -> "
+parse = map (\st -> (read $ head $ splitOn "," st, read $ splitOn "," st !! 1)) . endBy " -> "
 
 main :: IO()
 main = do
     inp <- map parse . lines <$> readFile "input.txt"
     let lst = map (, Rock) $ concatMap getPoints inp
-    let max = maximum $ map (snd . fst) lst
+    let maxR = maximum $ map (snd . fst) lst
     let m = Map.fromList lst
-    print $ check max (500, -1) m 0
+    print $ check maxR (500, -1) m 0
+
+    let minC = minimum $ map (fst . fst) lst
+    let maxC = maximum $ map (fst . fst) lst
+    let floor = map (, Floor) (getPoints [(minC - minC, maxR+2), (maxC+maxC, maxR+2)])
+    let m' = Map.fromList $ lst ++ floor
+    print $ check2 (500, -1) m' 0
