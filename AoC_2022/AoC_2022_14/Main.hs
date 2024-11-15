6497c1b3a6a1210906526de7cd5ce3ee7c243f4b
Lorin-Lange <lo.-lange@arcor.de>
2024-11-16 00:29:34 +0100

    updated solution of day 14 of AoC '22

--- AoC_2022/AoC_2022_14/Main.hs
+++ AoC_2022/AoC_2022_14/Main.hs
@@ -16,6 +16,8 @@ data Material = Rock | Sand | Floor
 
 type Grid = Map.Map (Int, Int) Material
 
+type Check = (Int, Int) -> Grid -> Int -> Int
+
 getPoints :: [(Int, Int)] -> [(Int, Int)]
 getPoints []  = []
 getPoints [_] = []
@@ -25,7 +27,7 @@ getPoints ((c1,r1):(c2,r2):xs)
     | r1 == r2 && c1 < c2 = map (,r1) [c1..c2] ++ getPoints ((c2,r2):xs)
     | r1 == r2 && c1 > c2 = map (,r1) [c2..c1] ++ getPoints ((c2,r2):xs)
 
-check :: ((Int, Int) -> Grid -> Int -> Int) -> (Int, Int) -> Grid -> Int -> Int
+check :: Check -> Check
 check f k@(c, r) g counter =
     case Map.lookup (c, r+1) g of
         (Just _) -> case Map.lookup (c-1, r+1) g of
@@ -35,12 +37,11 @@ check f k@(c, r) g counter =
                         Nothing  -> f (c-1, r+1) g counter
         Nothing  -> f (c, r+1) g counter
 
-check1 :: Int -> (Int, Int) -> Grid -> Int -> Int
-check1 max k@(c, r) g counter
-    | r > max   = counter
-    | otherwise = check (check1 max) k g counter
+check1 :: Int -> Check
+check1 max k@(c, r) g counter =
+    if r > max then counter else check (check1 max) k g counter
 
-check2 :: (Int, Int) -> Grid -> Int -> Int
+check2 :: Check
 check2 k@(c, r) g counter =
     case Map.lookup (500, 0) g of
         (Just _) -> counter
@@ -54,8 +55,8 @@ main = do
     inp <- map parse . lines <$> readFile "input.txt"
     let lst = map (, Rock) $ concatMap getPoints inp
     let maxR = maximum $ map (snd . fst) lst
-    let m = Map.fromList lst
-    putStrLn $ "Part 1: " ++ show (check1 maxR (500, -1) m 0)
+    
+    putStrLn $ "Part 1: " ++ show (check1 maxR (500, -1) (Map.fromList lst) 0)
 
     let minC = minimum $ map (fst . fst) lst
     let maxC = maximum $ map (fst . fst) lst
