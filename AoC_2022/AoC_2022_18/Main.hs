51ed556458998d69fdc7d9689628847adb63dc84
Lorin-Lange <lo.-lange@arcor.de>
2024-11-17 22:30:15 +0100

    added solution of the first part od day 18 of AoC '22

---
+++ AoC_2022/AoC_2022_18/Main.hs
@@ -0,0 +1,34 @@
+----------------------------------------------------
+--              Advent of Code 2022               --
+--            Day 18: Boiling Boulders            --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
+
+module Main where
+
+import Data.List.Split ( splitOn )
+
+adjacent :: [(Int, Int, Int)] -> Int
+adjacent []     = 0
+adjacent (x:xs) = length (filter (isAdjacent x) xs) + adjacent xs
+
+isAdjacent :: (Int, Int, Int) -> (Int, Int, Int) -> Bool
+isAdjacent (x1, y1, z1) (x2, y2, z2)
+    =  (x1 == x2 && y1 == y2 && (z1 == z2 - 1 || z1 == z2 + 1))
+    || (y1 == y2 && z1 == z2 && (x1 == x2 - 1 || x1 == x2 + 1))
+    || (z1 == z2 && x1 == x2 && (y1 == y2 - 1 || y1 == y2 + 1))
+
+surface :: [(Int, Int, Int)] -> Int
+surface lst = length lst * 6 - adjacent lst * 2
+
+parse :: String -> (Int, Int, Int)
+parse str = (read $ head lst, read $ lst !! 1, read $ lst !! 2)
+    where lst = splitOn "," str
+
+main :: IO ()
+main = do
+    inp <- map parse . lines <$> readFile "test_input.txt"
+    
+    putStrLn $ "Part 1: " ++ show (surface inp)
+
+    print "Result of part two"
