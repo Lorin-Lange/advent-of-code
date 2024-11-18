f4b5129ed0401924aaf9972537ba124f9e82e651
Lorin-Lange <lo.-lange@arcor.de>
2024-11-18 23:13:11 +0100

    refactored solution of day 14 of AoC '22

--- AoC_2022/AoC_2022_14/Main.hs
+++ AoC_2022/AoC_2022_14/Main.hs
@@ -10,6 +10,7 @@ module Main where
 
 import Data.List.Split ( endBy, splitOn )
 import qualified Data.Map as Map
+import Data.Maybe ( maybe )
 
 data Material = Rock | Sand | Floor
     deriving (Show, Eq)
@@ -28,21 +29,18 @@ getPoints ((c1,r1):(c2,r2):xs)
     | r1 == r2 && c1 > c2 = map (,r1) [c2..c1] ++ getPoints ((c2,r2):xs)
 
 check :: Check -> Check
-check f k@(c, r) g counter =
-    case Map.lookup (c, r+1) g of
-        (Just _) -> case Map.lookup (c-1, r+1) g of
-                        (Just _) -> case Map.lookup (c+1, r+1) g of
-                                        (Just _) -> f (500,-1) (Map.insert k Sand g) (counter + 1)
-                                        Nothing    -> f (c+1, r+1) g counter
-                        Nothing  -> f (c-1, r+1) g counter
-        Nothing  -> f (c, r+1) g counter
+check f k@(c, r) g counter = mbH (Map.lookup (c, r+1) g) (f (c, r+1) g counter) 
+                           . mbH (Map.lookup (c-1, r+1) g) (f (c-1, r+1) g counter)
+                           . mbH (Map.lookup (c+1, r+1) g) (f (500,-1) (Map.insert k Sand g) (counter + 1)) 
+                           $ f (c+1, r+1) g counter
+    where mbH (Just _) _ c = c; mbH Nothing  v _ = v
 
 check1 :: Int -> Check
-check1 max k@(c, r) g counter =
+check1 max k@(_, r) g counter =
     if r > max then counter else check (check1 max) k g counter
 
 check2 :: Check
-check2 k@(c, r) g counter =
+check2 k g counter =
     case Map.lookup (500, 0) g of
         (Just _) -> counter
         Nothing  -> check check2 k g counter
@@ -51,14 +49,13 @@ parse :: String -> [(Int, Int)]
 parse = map (\st -> (read $ head $ splitOn "," st, read $ splitOn "," st !! 1)) . endBy " -> "
 
 main :: IO()
-main = do
-    inp <- map parse . lines <$> readFile "input.txt"
-    let lst = map (, Rock) $ concatMap getPoints inp
-    let maxR = maximum $ map (snd . fst) lst
-    
-    putStrLn $ "Part 1: " ++ show (check1 maxR (500, -1) (Map.fromList lst) 0)
+main = do inp <- map parse . lines <$> readFile "input.txt"
+          let lst = map (, Rock) $ concatMap getPoints inp
+          let maxR = maximum $ map (snd . fst) lst
 
-    let minC = minimum $ map (fst . fst) lst
-    let maxC = maximum $ map (fst . fst) lst
-    let floor = map (, Floor) (getPoints [(minC - minC, maxR+2), (maxC + maxC, maxR+2)])
-    putStrLn $ "Part 2: " ++ show (check2 (500, -1) (Map.fromList $ lst ++ floor) 0)
+          putStrLn $ "Part 1: " ++ show (check1 maxR (500, -1) (Map.fromList lst) 0)
+
+          let minC = minimum $ map (fst . fst) lst
+          let maxC = maximum $ map (fst . fst) lst
+          let floor = map (, Floor) (getPoints [(minC - minC, maxR+2), (maxC + maxC, maxR+2)])
+          putStrLn $ "Part 2: " ++ show (check2 (500, -1) (Map.fromList $ lst ++ floor) 0)
