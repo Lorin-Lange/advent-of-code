67458216eca47f669640ad0bef855b57f27edaca
Lorin-Lange <lo.-lange@arcor.de>
2024-12-01 06:26:43 +0100

    added solution of day 1 of AoC '24

--- AoC_2024/AoC_2024_01/Main.hs
+++ AoC_2024/AoC_2024_01/Main.hs
@@ -1,7 +1,30 @@
+----------------------------------------------------
+--              Advent of Code 2024               --
+--           Day 1: Historian Hysteria            --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
 
 module Main where
 
+import Data.List.Split ( endBy )
+import Data.List ( sort, group )
+import Data.Maybe ( fromMaybe ) 
+
+makeScore :: [(Int, Int)] -> Int -> Int
+makeScore scores n = let score = fromMaybe 0 $ lookup n scores
+                     in score * n
+
+parse :: String -> (Int, Int)
+parse str = let lst = endBy "   " str
+            in (read $ head lst, read $ lst !! 1)
+
 main :: IO()
 main = do
-    input <- lines <$> readFile "test_input.txt"
-    print input
+    input <- map parse . lines <$> readFile "test_input.txt"
+    let lst1 = sort $ map fst input
+    let lst2 = sort $ map snd input
+
+    putStrLn $ "Part 1: " ++ show (sum $ zipWith (\a b -> abs $ a - b) lst1 lst2)
+
+    let scores = map (\lst -> (head lst, length lst)) $ group lst2
+    putStrLn $ "Part 2: " ++ show (sum $ map (makeScore scores) lst1)
