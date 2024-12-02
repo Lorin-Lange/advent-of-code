45927639a90d003676a1574f050f6985b071ffa3
Lorin-Lange <lo.-lange@arcor.de>
2024-12-02 07:49:22 +0100

    added part 1 of day 2 of AoC '24

--- AoC_2024/AoC_2024_02/Main.hs
+++ AoC_2024/AoC_2024_02/Main.hs
@@ -1,7 +1,24 @@
+----------------------------------------------------
+--              Advent of Code 2024               --
+--            Day 2: Red-Nosed Reports            --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
 
 module Main where
 
+isSafe :: [Int] -> Bool
+isSafe lst = monotonic && inRange
+    where ds = zipWith (-) (init lst) (tail lst)
+          monotonic = all (> 0) ds || all (< 0) ds
+          inRange = all (\x -> abs x <= 3) ds
+
+problemDampener :: [Int] -> [Int]
+problemDampener lst = lst
+
 main :: IO()
 main = do
-    input <- lines <$> readFile "test_input.txt"
-    print input
+    input <- map words . lines <$> readFile "test_input.txt"
+    let lst = map (map read) input
+
+    putStrLn $ "Part 1: " ++ show (length $ filter isSafe lst)
+    --putStrLn $ "Part 2: " ++ show (length $ filter isSafe $ map problemDampener lst)
