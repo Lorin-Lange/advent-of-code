e3f3f0b28d169e8f427a643cec03b6e2097492da
Lorin-Lange <lo.-lange@arcor.de>
2024-12-03 13:48:46 +0100

    refactored the solution of day 3 of AoC '24

--- AoC_2024/AoC_2024_03/Main.hs
+++ AoC_2024/AoC_2024_03/Main.hs
@@ -1,5 +1,5 @@
 ----------------------------------------------------
---              Advent of Code 2015               --
+--              Advent of Code 2024               --
 --              Day 3: Mull It Over               --
 --            Solution by Lorin Lange             --
 ----------------------------------------------------
@@ -12,23 +12,24 @@ import Text.Megaparsec.Char (char, string )
 import Data.Void ( Void )
 import Data.Either (fromRight)
 import qualified Text.Megaparsec.Char.Lexer as L
+import Data.Composition ( (.:) )
 
 type Parser = Parsec Void String
 
-data Mul = Mul Integer Integer | Do | Dont
+data Mul = Mul Integer | Do | Dont
     deriving Show
  
 parseInput1 :: Parser [Mul]
 parseInput1 = many $ try $ skipManyTill anySingle $ try mulCmd
     where mulCmd = between (string "mul(") (char ')') mul
-          mul = Mul <$> L.decimal <* char ',' <*> L.decimal
+          mul = Mul .: (*) <$> L.decimal <* char ',' <*> L.decimal
 
 parseInput2 :: Parser [Mul]
 parseInput2 = many $ try $ skipManyTill anySingle $ try cmd
-    where cmd = mulCmd <|> do' <|> dont
+    where cmd    = mulCmd <|> do' <|> dont
           mulCmd = between (string "mul(") (char ')') mul
-          mul = Mul <$> L.decimal <* char ',' <*> L.decimal
-          do' = string "do()" >> return Do
+          mul  = Mul .: (*) <$> L.decimal <* char ',' <*> L.decimal
+          do'  = string "do()" >> return Do
           dont = string "don't()" >> return Dont
 
 filterMul :: [Mul] -> [Mul]
@@ -36,16 +37,13 @@ filterMul lst = filterMulH lst True
     where filterMulH []        _ = []
           filterMulH (Do:xs)   _ = filterMulH xs True
           filterMulH (Dont:xs) _ = filterMulH xs False
-          filterMulH (x:xs) b | b = x : filterMulH xs b
-                              | otherwise = filterMulH xs b
+          filterMulH (x:xs)    b = if b then x : filterMulH xs b else filterMulH xs b
 
 main :: IO()
 main = do input <- readFile "input.txt"
 
-          let sumOfProducts = sum . map (\(Mul n1 n2) -> n1 * n2)
-
           let part1 = fromRight [] $ parse parseInput1 "" input
-          putStrLn $ "Part 1: " ++ show (sumOfProducts part1)
+          putStrLn $ "Part 1: " ++ show (sum $ map (\(Mul v) -> v) part1)
 
           let part2 = fromRight [] $ parse parseInput2 "" input
-          putStrLn $ "Part 2: " ++ show (sumOfProducts $ filterMul part2)
+          putStrLn $ "Part 2: " ++ show (sum $ map (\(Mul v) -> v) $ filterMul part2)
