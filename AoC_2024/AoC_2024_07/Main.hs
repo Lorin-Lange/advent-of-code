64ca3da5106fe329770be27033cae7531325816b
Lorin-Lange <lo.-lange@arcor.de>
2024-12-07 10:08:00 +0100

    added solution of day 7 of AoC '24

--- AoC_2024/AoC_2024_07/Main.hs
+++ AoC_2024/AoC_2024_07/Main.hs
@@ -1,7 +1,27 @@
+----------------------------------------------------
+--              Advent of Code 2024               --
+--              Day 7: Bridge Repair              --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
 
 module Main where
 
+import Data.List.Split ( splitOn )
+
+parse :: String -> (Integer, [Integer])
+parse str = (read value, nums)
+    where [value, lst] = splitOn ":" str
+          nums = map read $ splitOn " " $ tail lst
+
+isTestValue :: (Integer -> Integer -> [Integer]) -> (Integer, [Integer]) -> Bool
+isTestValue f (expected, nums) = expected `elem` makeNums [1] nums
+    where makeNums = foldl $ \is x -> concatMap (`f` x) is
+
 main :: IO()
-main = do
-    input <- lines <$> readFile "test_input.txt"
-    print input
+main = do input <- map parse . lines <$> readFile "test_input.txt"
+
+          let f1 n x = [n * x, n + x]
+          putStrLn $ "Part 1: " ++ show (sum . map fst $ filter (isTestValue f1) input)
+
+          let f2 n x = [n * x, n + x, read $ show n ++ show x]
+          putStrLn $ "Part 2: " ++ show (sum . map fst $ filter (isTestValue f2) input)
