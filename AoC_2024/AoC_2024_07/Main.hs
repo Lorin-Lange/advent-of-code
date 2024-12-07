2efd0ae149310e46503f1a0326454d18d2203e08
Lorin-Lange <lo.-lange@arcor.de>
2024-12-07 10:36:05 +0100

    added solution of day 7 of AoC '24

--- AoC_2024/AoC_2024_07/Main.hs
+++ AoC_2024/AoC_2024_07/Main.hs
@@ -14,14 +14,14 @@ parse str = (read value, nums)
           nums = map read $ splitOn " " $ tail lst
 
 isTestValue :: (Integer -> Integer -> [Integer]) -> (Integer, [Integer]) -> Bool
-isTestValue operators (expected, nums) = expected `elem` makeNums [1] nums
+isTestValue operators (expected, nums) = expected `elem` makeNums [-1] nums
     where makeNums = foldl $ \is x -> concatMap (`operators` x) is
 
 main :: IO()
-main = do input <- map parse . lines <$> readFile "test_input.txt"
+main = do input <- map parse . lines <$> readFile "input.txt"
 
-          let f1 n x = [n * x, n + x]
+          let f1 n x = if n == -1 then [x] else [n * x, n + x]
           putStrLn $ "Part 1: " ++ show (sum . map fst $ filter (isTestValue f1) input)
 
-          let f2 n x = [n * x, n + x, read $ show n ++ show x]
+          let f2 n x = if n == -1 then [x] else [n * x, n + x, read $ show n ++ show x]
           putStrLn $ "Part 2: " ++ show (sum . map fst $ filter (isTestValue f2) input)
