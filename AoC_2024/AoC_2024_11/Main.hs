a2c56fb201a83ff076bed9da8d4b8c529732d0c2
Lorin-Lange <lo.-lange@arcor.de>
2024-12-11 16:49:10 +0100

    updated the solution of day 11 of AoC '24

--- AoC_2024/AoC_2024_11/Main.hs
+++ AoC_2024/AoC_2024_11/Main.hs
@@ -10,19 +10,20 @@ module Main where
 
 import qualified Data.Map as Map
 
-type StoneMap = Map.Map Int Int -- Number and count of it.
+-- Number (key) and count of it (value).
+type StoneMap = Map.Map Int Int
 
 add :: Int -> Int -> StoneMap -> StoneMap
 add k v = Map.alter increaseCount k -- O(log n). 
-    where increaseCount Nothing   = Just v
-          increaseCount (Just v') = Just $ v + v'
+    where increaseCount Nothing    = Just v
+          increaseCount j@(Just _) = (+v) <$> j
 
 blinking :: StoneMap -> Int -> Int -> StoneMap
-blinking m 0 b = add 1 b m
-blinking m i b | odd len   = add (i * 2024) b m
-               | otherwise = (add i1 b . add i2 b) m
-    where len = length $ show i
-          (s1, s2) = splitAt (len `div` 2) (show i)
+blinking m 0 c = add 1 c m
+blinking m i c | even len  = (add i1 c . add i2 c) m
+               | otherwise = add (i * 2024) c m
+    where len      = length $ show i
+          (s1, s2) = splitAt (len `div` 2) $ show i
           (i1, i2) = (read s1, read s2)
 
 blink :: Int -> StoneMap -> Int
@@ -30,8 +31,8 @@ blink n = sum . Map.elems . last . take (n + 1) . iterate update
     where update = Map.foldlWithKey blinking Map.empty
 
 main :: IO()
-main = do input <- map read . words <$> readFile "input.txt"
-          let m = Map.fromList $ map (,1) input
+main = do lst <- map read . words <$> readFile "input.txt"
+          let m = Map.fromList $ map (,1) lst
 
           putStrLn $ "Part 1: " ++ show (blink 25 m)
           putStrLn $ "Part 2: " ++ show (blink 75 m)
