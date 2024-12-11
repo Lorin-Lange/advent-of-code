549edb04dd3150d4b6efe3c857f30dc603978efc
Lorin-Lange <lo.-lange@arcor.de>
2024-12-11 16:31:50 +0100

    added solution of day 11 of AoC '24

--- AoC_2024/AoC_2024_11/Main.hs
+++ AoC_2024/AoC_2024_11/Main.hs
@@ -1,7 +1,37 @@
+----------------------------------------------------
+--              Advent of Code 2024               --
+--           Day 11: Plutonian Pebbles            --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
+
+{-# LANGUAGE TupleSections #-}
 
 module Main where
 
+import qualified Data.Map as Map
+
+type StoneMap = Map.Map Int Int -- Number and count of it.
+
+add :: Int -> Int -> StoneMap -> StoneMap
+add k v = Map.alter increaseCount k -- O(log n). 
+    where increaseCount Nothing   = Just v
+          increaseCount (Just v') = Just $ v + v'
+
+blinking :: StoneMap -> Int -> Int -> StoneMap
+blinking m 0 b = add 1 b m
+blinking m i b | odd len   = add (i * 2024) b m
+               | otherwise = (add i1 b . add i2 b) m
+    where len = length $ show i
+          (s1, s2) = splitAt (len `div` 2) (show i)
+          (i1, i2) = (read s1, read s2)
+
+blink :: Int -> StoneMap -> Int
+blink n = sum . Map.elems . last . take (n + 1) . iterate update
+    where update = Map.foldlWithKey blinking Map.empty
+
 main :: IO()
-main = do
-    input <- lines <$> readFile "test_input.txt"
-    print input
+main = do input <- map read . words <$> readFile "input.txt"
+          let m = Map.fromList $ map (,1) input
+
+          putStrLn $ "Part 1: " ++ show (blink 25 m)
+          putStrLn $ "Part 2: " ++ show (blink 75 m)
