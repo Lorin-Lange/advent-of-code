9cd2b8233b3d14538536e962468a607d3ec2dc9c
Lorin-Lange <lo.-lange@arcor.de>
2024-12-12 18:11:36 +0100

    added input files

--- AoC_2024/AoC_2024_12/Main.hs
+++ AoC_2024/AoC_2024_12/Main.hs
@@ -1,7 +1,11 @@
+----------------------------------------------------
+--              Advent of Code 2024               --
+--             Day 12: Garden Groups              --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
 
 module Main where
 
 main :: IO()
-main = do
-    input <- lines <$> readFile "test_input.txt"
-    print input
+main = do input <- lines <$> readFile "test_input.txt"
+          print input
