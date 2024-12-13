79206bd44fb8318c4c7ba1d4e6b5b4247f46ab72
Lorin-Lange <lo.-lange@arcor.de>
2024-12-13 13:51:10 +0100

    refactored the solution of day 13 of AoC '24 slightly

--- AoC_2024/AoC_2024_13/Main.hs
+++ AoC_2024/AoC_2024_13/Main.hs
@@ -12,19 +12,20 @@ module Main where
 import Data.List.Split ( chunksOf, splitOn )
 import Data.Maybe ( mapMaybe )
 
-data Machine = M 
+data Machine = Machine 
     { ax, ay :: Int
     , bx, by :: Int
     , px, py :: Int }
 
 parse :: [String] -> [Machine]
-parse inp = map parseH $ chunksOf 4 inp where
-    parseH str = let [a1, a2] = splitOn "," $ head str
-                     [b1, b2] = splitOn "," $ str !! 1
-                     [p1, p2] = splitOn "," $ str !! 2
-                 in M { ax = read $ drop 12 a1, ay = read $ drop 3 a2,
-                        bx = read $ drop 12 b1, by = read $ drop 3 b2,
-                        px = read $ drop  9 p1, py = read $ drop 3 p2 }
+parse inp = map parseH $ chunksOf 4 inp 
+    where parseH str = Machine { 
+        ax = read $ drop 12 a1, ay = read $ drop 3 a2,
+        bx = read $ drop 12 b1, by = read $ drop 3 b2,
+        px = read $ drop  9 p1, py = read $ drop 3 p2 }
+            where [a1, a2] = splitOn "," $ head str
+                  [b1, b2] = splitOn "," $ str !! 1
+                  [p1, p2] = splitOn "," $ str !! 2
 
 solve :: [Machine] -> Int
 solve = sum . map (\(x, y) -> 3 * x + y) . mapMaybe solve'
@@ -34,7 +35,7 @@ solve = sum . map (\(x, y) -> 3 * x + y) . mapMaybe solve'
                   x = (m.px - m.bx * y) `div` m.ax
 
 unitConversion :: [Machine] -> [Machine]
-unitConversion = map $ \m -> m {px = m.px + offset, py = m.py + offset}
+unitConversion = map $ \m -> m { px = m.px + offset, py = m.py + offset }
     where offset = 10000000000000
 
 main :: IO()
