fbfb5e6990b0add7b8c5881f75fe102d279269d9
Lorin-Lange <lo.-lange@arcor.de>
2024-12-13 16:20:33 +0100

    refactored the solution of day 13 of AoC '24

--- AoC_2024/AoC_2024_13/Main.hs
+++ AoC_2024/AoC_2024_13/Main.hs
@@ -12,20 +12,22 @@ module Main where
 import Data.List.Split ( chunksOf, splitOn )
 import Data.Maybe ( mapMaybe )
 
-data Machine = Machine 
+data Machine = Machine
     { ax, ay :: Int
     , bx, by :: Int
     , px, py :: Int }
 
 parse :: [String] -> [Machine]
 parse = map parse' . chunksOf 4
-    where parse' str = Machine { 
-        ax = read $ drop 12 a1, ay = read $ drop 3 a2,
-        bx = read $ drop 12 b1, by = read $ drop 3 b2,
-        px = read $ drop  9 p1, py = read $ drop 3 p2 }
-            where [a1, a2] = splitOn "," $ head str
-                  [b1, b2] = splitOn "," $ str !! 1
-                  [p1, p2] = splitOn "," $ str !! 2
+    where parse' str = Machine {
+        ax = p 12 a1, ay = p 3 a2,
+        bx = p 12 b1, by = p 3 b2,
+        px = p  9 p1, py = p 3 p2 }
+            where p i s    = read $ drop i s
+                  split n  = splitOn "," $ str !! n
+                  [a1, a2] = split 0
+                  [b1, b2] = split 1
+                  [p1, p2] = split 2
 
 solve :: [Machine] -> Int
 solve = sum . map (\(x, y) -> 3 * x + y) . mapMaybe solve'
