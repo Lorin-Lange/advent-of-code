c7eaa381fe63f47c283251808c7a02c2a73a5bd1
Lorin-Lange <lo.-lange@arcor.de>
2024-12-14 17:29:38 +0100

    updated the solution of day 14 of AoC '24

--- AoC_2024/AoC_2024_14/Main.hs
+++ AoC_2024/AoC_2024_14/Main.hs
@@ -14,7 +14,7 @@ import Data.List.Split ( splitOn )
 import qualified Data.Map as Map
 import Data.Maybe ( fromMaybe )
 import Data.Ord ( comparing )
-import Data.List ( sortBy )
+import Data.List ( sortBy, sortOn )
 
 data Robot = Robot
     { px, py :: Int
@@ -22,14 +22,14 @@ data Robot = Robot
     deriving Show
 
 parse :: String -> [Robot]
-parse = map parse' . lines where 
+parse = map parse' . lines where
     parse' s = let [p, v]   = map (drop 2) $ splitOn " " s
                    [px, py] = map read $ splitOn "," p
                    [vx, vy] = map read $ splitOn "," v
                in Robot { px, py, vx, vy }
 
 move :: Int -> [Robot] -> [Robot]
-move t = map move' where 
+move t = map move' where
     move' r = let px = (r.px + t * r.vx) `mod` 101
                   py = (r.py + t * r.vy) `mod` 103
               in r { px, py }
@@ -41,22 +41,35 @@ safetyFactor rbts = product [q1, q2, q3, q4]
           q3 = sum [1 | r <- rbts, r.px < 50, r.py > 51]
           q4 = sum [1 | r <- rbts, r.px > 50, r.py > 51]
 
-printRobots :: [Robot] -> String
-printRobots lst = concatMap (\y -> map (\x -> lu (x, y)) [0..100] ++ "\n") [0..102]
-    where lu k = fromMaybe ' ' $ Map.lookup k (makeMap lst)
-
 makeMap :: [Robot] -> Map.Map (Int, Int) Char
 makeMap = Map.fromList . map (\r -> ((r.px, r.py), 'X'))
 
-computeRobots :: [Robot] -> [((Int, [Robot]), Int)]
-computeRobots rbts = sortBy (comparing snd) robots
-    where robots = map (\n -> ((n, move n rbts), safetyFactor $ move n rbts)) [1..101 * 103]
+variance :: [Int] -> Double
+variance lst = sum (map (\x -> (x - m)^2) l) / (n - 1)
+    where l = map fromIntegral lst
+          n = fromIntegral $ length l
+          m = sum l / n
+
+getMinVariance :: [Robot] -> (Robot -> Int) -> Int
+getMinVariance rbts s = fst . head . sortOn snd $ 
+    map (\t -> (t, variance . map s $ move t rbts)) [1..103]
+
+getFewestNumber :: [Robot] -> Int
+getFewestNumber rbts = bx + ((51 * (by-bx)) `mod` 103) * 101
+    where bx = getMinVariance rbts $ \r -> r.px
+          by = getMinVariance rbts $ \r -> r.py
+
+printRobots :: [Robot] -> String
+printRobots lst = concatMap (\y -> map (\x -> lu (x, y)) [0..100] ++ "\n") [0..102]
+    where lu k = fromMaybe ' ' . Map.lookup k $ makeMap lst
+
+getChristmasTree :: [Robot] -> Int -> IO()
+getChristmasTree rbts n = writeFile "christmas_tree.txt" $ printRobots tree
+    where tree = move n rbts
 
 main :: IO()
 main = do robots <- parse <$> readFile "input.txt"
 
           putStrLn $ "Part 1: " ++ show (safetyFactor $ move 100 robots)
-
-          let robots' = computeRobots robots
-          writeFile "tree.txt" $ printRobots (snd . fst $ head robots')
-          putStrLn $ "Part 2: " ++ show (fst . fst $ head robots')
+          putStrLn $ "Part 2: " ++ show (getFewestNumber robots)
+          getChristmasTree robots (getFewestNumber robots)
