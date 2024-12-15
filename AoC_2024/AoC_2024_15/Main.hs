3c5f29d4db6b44330f756a0544c444e4e33ff9d8
Lorin-Lange <lo.-lange@arcor.de>
2024-12-15 14:50:48 +0100

    added first part of day 15 of AoC '24

--- AoC_2024/AoC_2024_15/Main.hs
+++ AoC_2024/AoC_2024_15/Main.hs
@@ -8,20 +8,64 @@
 
 module Main where
 
+import qualified Data.Map as Map
 import Data.List.Split ( splitOn )
+
+type Pos = (Int, Int)
+
+type Grid = Map.Map Pos Item
+
+data Item = Robot | Box | Wall | Free
+    deriving (Show, Eq)
 
 data Dir = U | D | L | R
     deriving (Show, Eq)
 
--- (Grid, [Dir])
-parse :: [String] -> [String]
-parse str = let grid = splitOn [""] str !! 0
-                dirs = map dir $ concat $ splitOn [""] str !! 1
-            in grid
+parse :: [String] -> (Grid, [Dir])
+parse str = (grid, dirs)
+    where grid = makeGrid . head $ splitOn [""] str
+          dirs = map dir . concat $ splitOn [""] str !! 1
+          dir = \case '^' -> U; '>' -> R; '<' -> L; 'v' -> D
+          item = \case '#' -> Wall; '.' -> Free; '@' -> Robot; 'O' -> Box
+          makeGrid = Map.fromList . concatMap 
+                (\(y, l) -> zipWith (\ch x -> ((x, y), item ch)) l [0..]) . zip [0..]
 
-dir :: Char -> Dir
-dir = \case '^' -> U; '>' -> R; '<' -> L; 'v' -> D
+next :: Pos -> Dir -> Pos
+next (x, y) U = (x, y-1)
+next (x, y) D = (x, y+1)
+next (x, y) L = (x-1, y)
+next (x, y) R = (x+1, y)
+
+move :: (Pos, Grid) -> [Dir] -> (Pos, Grid)
+move = foldl move'
+
+move' :: (Pos, Grid) -> Dir -> (Pos, Grid)
+move' t@(p, g) d | not $ isPossible t d = t
+                 | otherwise            = (next p d, shift t d Robot)
+
+shift :: (Pos, Grid) -> Dir -> Item -> Grid
+shift (p, g) d i = case Map.lookup p g of
+    (Just Robot) -> shift (next p d, Map.insert p Free g) d Robot
+    (Just Box)   -> shift (next p d, Map.insert p i g) d Box
+    (Just Wall)  -> g
+    (Just Free)  -> Map.insert p i g
+
+isPossible :: (Pos, Grid) -> Dir -> Bool
+isPossible (p, g) d = case Map.lookup p g of
+    (Just Wall) -> False
+    (Just Free) -> True
+    (Just _)    -> isPossible (next p d, g) d
+    Nothing     -> False
+
+getPos :: Grid -> Pos
+getPos g = head [p | (p, Robot) <- Map.toList g]
+
+sumOfBoxes :: Grid -> Int
+sumOfBoxes g = sum [100 * y + x | ((x,y), Box) <- Map.toList g]
 
 main :: IO()
-main = do input <- lines <$> readFile "mini_input.txt"
-          print $ parse input
+main = do t@(grid, dirs) <- parse . lines <$> readFile "input.txt"
+          let s = getPos grid
+          print s
+          let (_, g) = move (s, grid) dirs
+          print $ sumOfBoxes g
