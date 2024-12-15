88ebae2530efda1e3bbc1f12b45f838dc6780f1e
Lorin-Lange <lo.-lange@arcor.de>
2024-12-15 12:01:22 +0100

    added input for day 15 of AoC '24

--- AoC_2024/AoC_2024_15/Main.hs
+++ AoC_2024/AoC_2024_15/Main.hs
@@ -1,7 +1,27 @@
+----------------------------------------------------
+--              Advent of Code 2024               --
+--             Day 15: Warehouse Woes             --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
+
+{-# Language LambdaCase #-}
 
 module Main where
 
+import Data.List.Split ( splitOn )
+
+data Dir = U | D | L | R
+    deriving (Show, Eq)
+
+-- (Grid, [Dir])
+parse :: [String] -> [String]
+parse str = let grid = splitOn [""] str !! 0
+                dirs = map dir $ concat $ splitOn [""] str !! 1
+            in grid
+
+dir :: Char -> Dir
+dir = \case '^' -> U; '>' -> R; '<' -> L; 'v' -> D
+
 main :: IO()
-main = do
-    input <- lines <$> readFile "test_input.txt"
-    print input
+main = do input <- lines <$> readFile "mini_input.txt"
+          print $ parse input
