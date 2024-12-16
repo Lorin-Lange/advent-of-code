b6ebd35c5eb9fe721d5af61fb1ff8465cd853ad9
Lorin-Lange <lo.-lange@arcor.de>
2024-12-16 20:46:18 +0100

    added input files for day 16 of AoC '24

--- AoC_2024/AoC_2024_16/Main.hs
+++ AoC_2024/AoC_2024_16/Main.hs
@@ -1,7 +1,24 @@
+----------------------------------------------------
+--              Advent of Code 2024               --
+--             Day 16: Reindeer Maze              --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
 
 module Main where
 
+import qualified Data.Map as Map
+
+
+
+
+makeGrid :: (Char -> a) -> [String] -> Map.Map (Int, Int) a
+makeGrid convert = Map.fromList . concatMap (\(y, l) -> zipWith (\ch x -> ((x, y), convert ch)) l [0..]) . zip [0..]
+
 main :: IO()
-main = do
-    input <- lines <$> readFile "test_input.txt"
-    print input
+main = do input <- lines <$> readFile "test_input.txt"
+          print input
+
+          putStrLn $ "Part 1: " ++ show "TODO"
+          putStrLn $ "Part 2: " ++ show "TODO"
+
+
