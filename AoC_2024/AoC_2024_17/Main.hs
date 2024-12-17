bc68d0b79174cc2435836dc1c3bc73dea9566131
Lorin-Lange <lo.-lange@arcor.de>
2024-12-17 17:29:52 +0100

    added first part of day 17 of AoC '24

--- AoC_2024/AoC_2024_17/Main.hs
+++ AoC_2024/AoC_2024_17/Main.hs
@@ -1,7 +1,55 @@
+----------------------------------------------------
+--              Advent of Code 2024               --
+--         Day 17: Chronospatial Computer         --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
+
+{-# LANGUAGE OverloadedRecordDot   #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns        #-}
 
 module Main where
 
+import Data.Bits ( shiftR, xor , (.&.) )
+import Data.List.Split ( splitOn )
+
+data Registers = R { a, b, c :: Int }
+    deriving Show
+
+parse :: [String] -> (Registers, [Int])
+parse lst = (R { a, b, c }, instr)
+    where a = read $ splitOn ": " (head lst) !! 1
+          b = read $ splitOn ": " (lst !! 1) !! 1
+          c = read $ splitOn ": " (lst !! 2) !! 1
+          instr = map read . splitOn "," . drop 9 $ lst !! 4
+
+run :: (Registers, [Int]) -> String
+run (rs, instr) = show out where (_, _, out) = runH (rs, 0, []) instr
+
+runH :: (Registers, Int, [Int]) -> [Int] -> (Registers, Int, [Int])
+runH t@(rs, ip, out) instr = if ip >= length instr then t
+                           else runH (execute instruction ip operand rs out) instr
+        where instruction = instr !! ip
+              operand = instr !! (ip + 1)
+
+-- opcode ptr rs -> (rs, ptr)
+execute :: Int -> Int -> Int -> Registers -> [Int] -> (Registers, Int, [Int])
+execute 0 ip op rs out = (rs { a = rs.a `shiftR` comboOp op rs }, ip + 2, out)
+execute 1 ip op rs out = (rs { b = rs.b `xor` op }, ip + 2, out)
+execute 2 ip op rs out = (rs { b = comboOp op rs .&. 7 }, ip + 2, out)
+execute 3 ip op rs out = (rs, if rs.a == 0 then ip + 2 else op, out)
+execute 4 ip op rs out = (rs { b = rs.b `xor` rs.c }, ip + 2, out)
+execute 5 ip op rs out = (rs, ip + 2, out ++ [comboOp op rs .&. 7])
+execute 6 ip op rs out = (rs { b = rs.a  `shiftR` comboOp op rs }, ip + 2, out)
+execute 7 ip op rs out = (rs { c = rs.a `shiftR` comboOp op rs }, ip + 2, out)
+
+comboOp :: Int -> Registers -> Int
+comboOp 4 rs = rs.a
+comboOp 5 rs = rs.b
+comboOp 6 rs = rs.c
+comboOp o rs | 0 <= o && o <= 3 = o
+
 main :: IO()
-main = do
-    input <- lines <$> readFile "test_input.txt"
-    print input
+main = do t@(rs, cmds) <- parse . lines <$> readFile "input.txt"
+          putStrLn $ "Part 1: " ++ (tail . init $ run t)
+          putStrLn $ "Part 2: " ++ show ("TODO")
