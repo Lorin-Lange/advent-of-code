03f430dca28261e27ed98086eb07b27386cf8e3b
Lorin-Lange <lo.-lange@arcor.de>
2024-12-20 18:41:17 +0100

    added input files

--- AoC_2024/AoC_2024_19/Main.hs
+++ AoC_2024/AoC_2024_19/Main.hs
@@ -1,7 +1,12 @@
+----------------------------------------------------
+--              Advent of Code 2024               --
+--              Day 19: Linen Layout              --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
 
 module Main where
 
 main :: IO()
-main = do
-    input <- lines <$> readFile "test_input.txt"
-    print input
+main = do input <- lines <$> readFile "test_input.txt"
+          putStrLn $ "Part 1: " ++ show ("")
+          putStrLn $ "Part 2: " ++ show ("")
