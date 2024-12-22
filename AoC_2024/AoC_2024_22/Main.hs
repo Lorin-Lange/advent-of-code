fb346086e7ed76a1b80d299d78d0efb110b99bbd
Lorin-Lange <lo.-lange@arcor.de>
2024-12-22 15:08:10 +0100

    updated first part of day 22 of AoC '24

--- AoC_2024/AoC_2024_22/Main.hs
+++ AoC_2024/AoC_2024_22/Main.hs
@@ -7,24 +7,23 @@
 module Main where
 
 import Data.Bits ( xor )
-
-mix :: Integer -> Integer -> Integer
-mix sn gv = sn `xor` gv
-
-prune :: Integer -> Integer
-prune n = n `mod` 16_777_216
+import Data.Composition ( (.:) )
 
 next :: Integer -> Integer
-next n = prune $ mix step2 (step2 * 2048)
-    where step1 = prune $ mix n (64 * n)
-          step2 = prune $ mix step1 (step1 `div` 32)
+next = fun (* 2048) . fun (`div` 32) . fun (* 64)
+    where mixAndPrune = (`mod` 16_777_216) .: xor
+          fun f i = mixAndPrune i $ f i
 
-newSecretNumber :: Integer -> Integer
-newSecretNumber = last . take 2001 . iterate next
+secretNumber2000 :: Integer -> Integer
+secretNumber2000 = last . take 2001 . iterate next
+
+priceChanges :: Integer -> [Integer]
+priceChanges n = zipWith (-) (tail prices) prices
+    where secrets = take 2001 $ iterate next n
+          prices  = map (`mod` 10) secrets
 
 main :: IO()
 main = do lst <- map read . lines <$> readFile "input.txt"
-
-          putStrLn $ "Part 1: " ++ show (sum $ map newSecretNumber lst)
+          putStrLn $ "Part 1: " ++ show (sum $ map secretNumber2000 lst)
 
           putStrLn $ "Part 2: " ++ show ("")
