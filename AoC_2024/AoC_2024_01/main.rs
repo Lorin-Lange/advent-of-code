18339f04b3ef44ca6f333e54632ca02eb33dc6d5
Lorin-Lange <lo.-lange@arcor.de>
2025-01-22 18:20:20 +0100

    refactored solution in Rust of day 1 of AoC '24

--- AoC_2024/AoC_2024_01/main.rs
+++ AoC_2024/AoC_2024_01/main.rs
@@ -9,9 +9,8 @@ use std::collections::HashMap;
 
 fn parse(path : &str) -> (Vec<i32>, Vec<i32>) {
     let input = fs::read_to_string(path).expect("Couldn't read file.");
-    let lines = input.lines().collect::<Vec<_>>();
     let (mut l1, mut l2) = (Vec::new(), Vec::new());
-    for line in lines {
+    for line in input.lines() {
         let mut st = line.split_whitespace();
         let mut n = st.next().unwrap().parse().unwrap();
         l1.push(n);
@@ -32,25 +31,31 @@ fn part_2(l1 : &Vec<i32>, l2 : &Vec<i32>) -> i32 {
     for &x in l2 {
         *map.entry(x).or_insert(0) += 1;
     }
-    l1.iter().map(|&a| a * map.get(&a).unwrap_or(&0)).sum()
-}
-
-#[test]
-fn test_input() {
-    let (lst1, lst2) = parse("test_input.txt");
-    assert_eq!(part_1(&lst1, &lst2), 11);
-    assert_eq!(part_2(&lst1, &lst2), 31);
-}
-
-#[test]
-fn input() {
-    let (lst1, lst2) = parse("input.txt");
-    assert_eq!(part_1(&lst1, &lst2), 2430334);
-    assert_eq!(part_2(&lst1, &lst2), 28786472);
+    l1.iter().map(|&x| x * map.get(&x).unwrap_or(&0)).sum()
 }
 
 fn main() {
     let (lst1, lst2) = parse("input.txt");
     println!("Part 1: {}", part_1(&lst1, &lst2));
     println!("Part 2: {}", part_2(&lst1, &lst2));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        let (lst1, lst2) = parse("test_input.txt");
+        assert_eq!(part_1(&lst1, &lst2), 11);
+        assert_eq!(part_2(&lst1, &lst2), 31);
+    }
+
+    #[test]
+    fn input() {
+        let (lst1, lst2) = parse("input.txt");
+        assert_eq!(part_1(&lst1, &lst2), 2430334);
+        assert_eq!(part_2(&lst1, &lst2), 28786472);
+    }
+
 }
