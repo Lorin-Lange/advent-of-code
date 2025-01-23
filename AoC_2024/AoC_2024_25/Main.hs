5e7115dde4fe7eac96004b10f2ec556f50210864
Lorin-Lange <lo.-lange@arcor.de>
2025-01-23 03:23:31 +0100

    refactored sulution of day 25 of AoC '24

--- AoC_2024/AoC_2024_25/Main.hs
+++ AoC_2024/AoC_2024_25/Main.hs
@@ -11,14 +11,11 @@ module Main where
 import Data.List.Split ( splitOn )
 import Data.List ( partition, transpose )
 
-parse :: ([[String]], [[String]]) -> ([[Int]], [[Int]])
-parse (l1, l2) = (applyCount l1, applyCount $ map reverse l2)
-    where applyCount = map $ map ((\n -> n - 1) . count) . transpose
-          count = \case [] -> 0; ('.':_) -> 0; ('#':xs) -> 1 + count xs
-
-schematics :: [String] -> ([[String]], [[String]])
-schematics = partition arePins . splitOn [""]
-    where arePins = all (== '#') . head
+parse :: [String] -> ([[Int]], [[Int]])
+parse lst = (count l1, count $ map reverse l2)
+    where (l1, l2) = partition (all (== '#') . head) $ splitOn [""] lst
+          count = map $ map count' . transpose
+          count' = \case [] -> -1; ('.':_) -> -1; ('#':xs) -> 1 + count' xs
 
 fit :: ([Int], [Int]) -> Bool
 fit (l1, l2) = all (<= 5) $ zipWith (+) l1 l2
@@ -27,5 +24,5 @@ makeComb :: ([[Int]], [[Int]]) -> [([Int], [Int])]
 makeComb (l1, l2) = l1 >>= \l1' -> l2 >>= \l2' -> pure (l1', l2')
 
 main :: IO()
-main = do combi <- makeComb . parse . schematics . lines <$> readFile "input.txt"
+main = do combi <- makeComb . parse . lines <$> readFile "input.txt"
           putStrLn $ "Part 1: " ++ show (length $ filter fit combi)
