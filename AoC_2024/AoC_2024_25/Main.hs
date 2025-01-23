d63fd421e723671921f5c43717d73fd1edd34d04
Lorin-Lange <lo.-lange@arcor.de>
2025-01-23 02:59:28 +0100

    added sulution of day 25 of AoC '24

--- AoC_2024/AoC_2024_25/Main.hs
+++ AoC_2024/AoC_2024_25/Main.hs
@@ -1,7 +1,31 @@
+----------------------------------------------------
+--              Advent of Code 2024               --
+--             Day 25: Code Chronicle             --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
+
+{-# Language LambdaCase #-}
 
 module Main where
 
+import Data.List.Split ( splitOn )
+import Data.List ( partition, transpose )
+
+parse :: ([[String]], [[String]]) -> ([[Int]], [[Int]])
+parse (l1, l2) = (applyCount l1, applyCount $ map reverse l2)
+    where applyCount = map $ map ((\n -> n - 1) . count) . transpose
+          count = \case [] -> 0; ('.':_) -> 0; ('#':xs) -> 1 + count xs
+
+schematics :: [String] -> ([[String]], [[String]])
+schematics = partition arePins . splitOn [""]
+    where arePins = all (== '#') . head
+
+fit :: ([Int], [Int]) -> Bool
+fit (l1, l2) = all (<= 5) $ zipWith (+) l1 l2
+
+makeComb :: ([[Int]], [[Int]]) -> [([Int], [Int])]
+makeComb (l1, l2) = l1 >>= \l1' -> l2 >>= \l2' -> pure (l1', l2')
+
 main :: IO()
-main = do
-    input <- lines <$> readFile "test_input.txt"
-    print input
+main = do combi <- makeComb . parse . schematics . lines <$> readFile "input.txt"
+          putStrLn $ "Part 1: " ++ show (length $ filter fit combi)
---
+++ AoC_2024/AoC_2024_25/main.rs
@@ -0,0 +1,9 @@
+////////////////////////////////////////////////////
+//              Advent of Code 2024               //
+//             Day 25: Code Chronicle             //
+//            Solution by Lorin Lange             //
+////////////////////////////////////////////////////
+
+fn main() {
+    println!("Hello World!");
+}
