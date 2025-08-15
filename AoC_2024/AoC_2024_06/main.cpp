0bcfbb4c55d24b88a9130d5726be8c45def750af
Lorin-Lange <lo.-lange@arcor.de>
2025-08-15 22:03:58 +0200

    updated the cpp solution of day 6 of AoC '24

--- AoC_2024/AoC_2024_06/main.cpp
+++ AoC_2024/AoC_2024_06/main.cpp
@@ -14,11 +14,11 @@
 
 using namespace std;
 
-pair<int, int> find_start(vector<string> &g) {
+auto find_start(vector<string> &g) {
     for(int r = 0; r < g.size(); r++) {
         for(int c = 0; c < g[r].size(); c++) {
             if(g[r][c] == '^') {
-                g[r][c] == '.';
+                g[r][c] = '.';
                 return make_pair(r, c);
             }
         }
@@ -26,26 +26,29 @@ pair<int, int> find_start(vector<string> &g) {
     return make_pair(-1, -1);
 }
 
-int part_one(pair<int, int> pos, vector<string> &grid) {
+int part_one(pair<int, int> pos, vector<string> &g) {
     vector<pair<int, int>> dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    int dir = 0;
-    int height = grid.size();
-    int width = grid[0].size();
+    int height = g.size();
+    int width = g[0].size();
     set<pair<int, int>> visited;
-    pair<int, int> next_dir;
+    int dir = 0;
     while(true) {
-        next_dir = dirs[dir];
-        int r = pos.first + next_dir.first;
-        int c = pos.second + next_dir.second;
+        visited.insert(pos);
+        int r = pos.first + dirs[dir].first;
+        int c = pos.second + dirs[dir].second;
         if(r < 0 || r >= height || c < 0 || c >= width) break;
-        if (grid[r][c] == '.') {
+        if(g[r][c] == '.') {
             pos = {r, c};
-            visited.insert(pos);
-        } else {
+        }
+        else {
             dir = (dir + 1) % 4;
         }
     }
     return visited.size();
+}
+
+int part_two(pair<int, int> pos, vector<string> g) { 
+    return -1; 
 }
 
 int main() {
@@ -65,17 +68,11 @@ int main() {
         cout << "Problem while opening the file." << endl;
         return EXIT_FAILURE;
     }
+
     auto start_pos = find_start(grid);
 
-    cout << "Part 1: ";
-    cout << part_one(start_pos, grid) << "\n";
-
-    cout << "Part 2: ";
-    cout << "\n";
-
-
-
-
+    cout << "Part 1: " << part_one(start_pos, grid) << "\n";
+    cout << "Part 2: " << part_two(start_pos, grid) << "\n";
 
     return EXIT_SUCCESS;
 }
