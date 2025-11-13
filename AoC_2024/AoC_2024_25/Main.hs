8eaf54726cd233829a2ed668536940ae3ada4d54
Lorin-Lange <lo.-lange@arcor.de>
2025-11-13 11:10:06 +0100

    refactored solution of day 25 of AoC '24

--- AoC_2024/AoC_2024_25/Main.hs
+++ AoC_2024/AoC_2024_25/Main.hs
@@ -8,21 +8,18 @@
 
 module Main where
 
-import Data.List.Split ( splitOn )
-import Data.List ( partition, transpose )
+import Data.List.Split (splitOn)
+import Data.List (partition, transpose)
+import Control.Monad (join, liftM2)
+import Control.Arrow ((***))
+import Data.Bifunctor (second)
 
 parse :: [String] -> ([[Int]], [[Int]])
-parse lst = (count l, count $ map reverse k)
-    where count  = map $ map count' . transpose
-          count' = \case [] -> -1; ('.':_) -> -1; ('#':t) -> 1 + count' t
-          (l, k) = partition (all (== '#') . head) $ splitOn [""] lst
-
-fit :: ([Int], [Int]) -> Bool
-fit = all (<= 5) . uncurry (zipWith (+))
-
-combi :: ([[Int]], [[Int]]) -> [([Int], [Int])]
-combi (l1, l2) = l1 >>= \l1' -> l2 >>= \l2' -> [(l1', l2')]
+parse lst = join (***) (map $ map c . transpose) $ second (map reverse) t
+    where t = partition (all (== '#') . head) $ splitOn [""] lst
+          c = \case [] -> -1; ('.':_) -> -1; ('#':t) -> 1 + c t
 
 main :: IO()
-main = do cs <- combi . parse . lines <$> readFile "input.txt"
-          putStrLn $ "Part 1: " ++ show (length $ filter fit cs)
+main = do cs <- uncurry (liftM2 (,)) . parse . lines <$> readFile "input.txt"
+          let fits = all (<= 5) . uncurry (zipWith (+))
+          putStrLn $ "Part 1: " ++ show (length $ filter fits cs)
