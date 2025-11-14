b3e22cc7e8d6b08dfdea2f8a91c7de72ead1e8f3
Lorin-Lange <lo.-lange@arcor.de>
2025-11-14 10:24:48 +0100

    added hs solution of day 19 of AoC '24

--- AoC_2024/AoC_2024_19/Main.hs
+++ AoC_2024/AoC_2024_19/Main.hs
@@ -6,7 +6,19 @@
 
 module Main where
 
+import Data.List (isPrefixOf)
+import Data.List.Split (splitOn)
+import Data.MemoTrie (memoFix)
+import Control.Arrow (Arrow((&&&)))
+
+ways :: [String] -> String -> Int
+ways ts = memoFix go where
+  go _   ""   = 1
+  go fix dsgn = sum [fix (drop (length t) dsgn) 
+                    | t <- ts, t `isPrefixOf` dsgn]
+
 main :: IO()
-main = do input <- lines <$> readFile "test_input.txt"
-          putStrLn $ "Part 1: " ++ show ("")
-          putStrLn $ "Part 2: " ++ show ("")
+main = do input <- lines <$> readFile "input.txt"
+          let (ps, dd) = splitOn ", " . head &&& drop 2 $ input
+          putStrLn $ "Part 1: " ++ show (length $ filter ((>0) . ways ps) dd)
+          putStrLn $ "Part 2: " ++ show (sum $ map (ways ps) dd)
