a0a2f0fc11f7f92dd5e10f1bb299e5f26dd84942
Lorin-Lange <lo.-lange@arcor.de>
2025-11-15 14:10:33 +0100

    updated solution of day 19 of AoC '24

--- AoC_2024/AoC_2024_19/Main.hs
+++ AoC_2024/AoC_2024_19/Main.hs
@@ -14,11 +14,10 @@ import Control.Arrow ((&&&))
 ways :: [String] -> String -> Int
 ways ts = memoFix go where
   go _   ""   = 1
-  go fix dsgn = sum [fix $ drop (length t) dsgn 
-                    | t <- ts, t `isPrefixOf` dsgn]
+  go fix dsgn = sum [fix $ drop (length t) dsgn | t <- ts, t `isPrefixOf` dsgn]
 
 main :: IO()
-main = do input <- lines <$> readFile "test_input.txt"
+main = do input <- lines <$> readFile "input.txt"
           let (ps, dd) = splitOn ", " . head &&& drop 2 $ input
           putStrLn $ "Part 1: " ++ show (sum [1 | d <- dd, ways ps d > 0])
           putStrLn $ "Part 2: " ++ show (sum [ways ps d | d <- dd])
