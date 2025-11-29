6b5120a7841f6de01ceac02138b221073ae9d5a3
Lorin-Lange <lo.-lange@arcor.de>
2025-11-29 17:17:15 +0100

    deleted old project

--- AoC_2025/advent-of-code-2025/src/lib.rs
+++
@@ -1,9 +0,0 @@
-////////////////////////////////////////////////////
-//              Advent of Code 2025               //
-//                Common functions                //
-//            Solution by Lorin Lange             //
-////////////////////////////////////////////////////
-
-pub fn test_fun() -> &'static str {
-    "Advent of Code 2025!"
-}
