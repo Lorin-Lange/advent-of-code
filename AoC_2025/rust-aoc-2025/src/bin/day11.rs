7ca76b39ac21836ef6a4fc61e3d698843dc44833
Lorin-Lange <lo.-lange@arcor.de>
2025-11-29 17:09:06 +0100

    renamed project

---
+++ AoC_2025/rust-aoc-2025/src/bin/day11.rs
@@ -0,0 +1,28 @@
+fn part_1() {
+
+}
+
+fn part_2() {
+}
+
+
+fn main() {
+    let input = include_str!("../../inputs/day11/input.txt");
+    println!("Part 1: {}", input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(true, true);
+    }
+
+    #[test]
+    fn test_part_2() {
+    }
+
+}
+
---
+++ AoC_2025/rust-aoc-2025/src/lib.rs
@@ -0,0 +1,9 @@
+////////////////////////////////////////////////////
+//              Advent of Code 2025               //
+//                Common functions                //
+//            Solution by Lorin Lange             //
+////////////////////////////////////////////////////
+
+pub fn test_fun() -> &'static str {
+    "Advent of Code 2025!"
+}
