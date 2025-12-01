971518a45c548ad65306b185345c2f111b2ca1b8
Lorin-Lange <lo.-lange@arcor.de>
2025-12-01 07:46:45 +0100

    added first part of day 1 of AoC '25

--- AoC_2025/hs/AoC_2025_01/Main.hs
+++ AoC_2025/hs/AoC_2025_01/Main.hs
@@ -1,4 +1,20 @@
+----------------------------------------------------
+--              Advent of Code 2025               --
+--             Day 1: Secret Entrance             --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
+
+parse :: String -> Int
+parse ('L':n) = negate $ read n
+parse ('R':n) = read n
+parse _       = error "invalid format"
+
+rotation :: Integral a => a -> a -> a
+rotation n m = (n + m) `mod` 100
+
+moves :: [Int] -> [Int]
+moves = scanl rotation 50
 
 main :: IO()
-main = putStrLn "Advent of Code 2025"
-
+main = do input <- map parse . lines <$> readFile "input.txt"
+          putStrLn $ "Part 1: " ++ show (sum [1 | n <- moves input, n == 0])
