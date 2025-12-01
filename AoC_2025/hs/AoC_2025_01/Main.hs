eaadf12a1691ddc380480d68b946a4880d406426
Lorin-Lange <lo.-lange@arcor.de>
2025-12-01 18:10:43 +0100

    added the second part of the Haskell solution for day 1 of AoC '25

--- AoC_2025/hs/AoC_2025_01/Main.hs
+++ AoC_2025/hs/AoC_2025_01/Main.hs
@@ -4,17 +4,27 @@
 --            Solution by Lorin Lange             --
 ----------------------------------------------------
 
+import Data.List (mapAccumL)
+
 parse :: String -> Int
 parse ('L':n) = negate $ read n
 parse ('R':n) = read n
-parse _       = error "invalid format"
 
-rotation :: Integral a => a -> a -> a
-rotation n m = (n + m) `mod` 100
+rot :: Int -> Int -> Int
+rot n m = (n + m) `mod` 100
 
-moves :: [Int] -> [Int]
-moves = scanl rotation 50
+atZero :: Int -> Int -> Int
+atZero n m | m > 0     = (n + m) `div` 100
+           | otherwise = (n + m) `div` (-100) - n `div` (-100)
+
+part1 :: [Int] -> Int
+part1 lst = sum [1 | n <- scanl rot 50 lst, n == 0]
+
+part2 :: [Int] -> Int
+part2 = sum . snd . mapAccumL step 50
+  where step n m = (rot n m, atZero n m)
 
 main :: IO()
 main = do input <- map parse . lines <$> readFile "input.txt"
-          putStrLn $ "Part 1: " ++ show (sum [1 | n <- moves input, n == 0])
+          putStrLn $ "Part 1: " ++ show (part1 input)
+          putStrLn $ "Part 2: " ++ show (part2 input)
