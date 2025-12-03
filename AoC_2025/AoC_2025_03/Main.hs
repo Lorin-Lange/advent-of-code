bbcc61da88f412f74b6b856e30315e08ffb65746
Lorin-Lange <lo.-lange@arcor.de>
2025-12-03 15:08:53 +0100

    refactored solution of day 3 of AoC '25

--- AoC_2025/AoC_2025_03/Main.hs
+++ AoC_2025/AoC_2025_03/Main.hs
@@ -6,16 +6,8 @@
 
 module Main (main) where
 
-import Data.Char (digitToInt)
-import Data.List (foldl')
-
-maxBank :: String -> Int
-maxBank = snd . foldl' step (-1, 0) . map digitToInt
-  where step (-1, res) d = (d, res)
-        step (m,  res) d = (max m d, max res $ m * 10 + d)
-
-best12 :: String -> Integer
-best12 = read . go 12
+bestN :: Int -> String -> Integer
+bestN n = read . go n
   where go 0 _  = []
         go n xs = c : go (n - 1) rest
           where c           = maximum $ take (length xs - n + 1) xs
@@ -23,5 +15,5 @@ best12 = read . go 12
 
 main :: IO()
 main = do input <- lines <$> readFile "input.txt"
-          putStrLn $ "Part 1: " ++ show (sum $ map maxBank input)
-          putStrLn $ "Part 2: " ++ show (sum $ map best12 input)
+          putStrLn $ "Part 1: " ++ show (sum $ map (bestN  2) input)
+          putStrLn $ "Part 2: " ++ show (sum $ map (bestN 12) input)
