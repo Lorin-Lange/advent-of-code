4bfc9beb55a4fa43e58a34a8c37ad3a9928f8cd1
Lorin-Lange <lo.-lange@arcor.de>
2025-12-04 16:56:36 +0100

    updated README

--- AoC_2025/AoC_2025_03/Main.hs
+++ AoC_2025/AoC_2025_03/Main.hs
@@ -6,12 +6,12 @@
 
 module Main (main) where
 
-bestN :: Int -> String -> Integer
+bestN :: Int -> String -> Int
 bestN n = read . go n
   where go 0 _  = []
-        go n xs = c : go (n - 1) rest
-          where c           = maximum $ take (length xs - n + 1) xs
-                (_, _:rest) = break (== c) xs
+        go n xs = c : go (n - 1) r
+          where c        = maximum $ take (length xs - n + 1) xs
+                (_, _:r) = break (== c) xs
 
 main :: IO()
 main = do input <- lines <$> readFile "input.txt"
