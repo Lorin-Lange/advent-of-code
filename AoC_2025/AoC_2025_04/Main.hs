afebe5981811a2b19780fdaf3ad3a6203ab8c527
Lorin-Lange <lo.-lange@arcor.de>
2025-12-04 16:52:37 +0100

    added solution for day 4 of AoC '25

---
+++ AoC_2025/AoC_2025_04/Main.hs
@@ -0,0 +1,47 @@
+----------------------------------------------------
+--              Advent of Code 2025               --
+--           Day 4: Printing Department           --
+--            Solution by Lorin Lange             --
+----------------------------------------------------
+
+module Main (main) where
+
+import Data.List.Extra ((!?))
+import Control.Lens ((&), (.~), Ixed(ix))
+
+type Pos  = (Int, Int)
+
+type Grid = [[Char]]
+
+neighbors :: Grid -> Pos -> Int
+neighbors g (x, y) = sum 
+   [ isRoll g (x + dx, y + dy)
+   | dx <- [-1..1], dy <- [-1..1]
+   , (dx, dy) /= (0, 0)]
+
+isRoll :: Grid -> Pos -> Int
+isRoll g (x, y) = maybe 0 (fromEnum . (== '@')) (g !? y >>= (!? x))
+
+accessible :: Grid -> Pos -> Bool
+accessible g p = Just '@' == (g !? snd p >>= (!? fst p))
+                 && neighbors g p < 4
+
+remove :: Grid -> Pos -> Grid
+remove g (x, y) = g & ix y . ix x .~ '.'
+
+step :: Grid -> (Grid, Int)
+step g = (foldl remove g ps, length ps)
+   where ps = [(x, y) | y <- r, x <- r, accessible g (x, y)]
+         r  = [0..length g - 1]
+
+simulate :: Grid -> Int
+simulate g = go g 0
+   where go grid total = 
+          case step grid of
+            (_, 0)     -> total
+            (grid', n) -> go grid' $ total + n
+
+main :: IO()
+main = do grid <- lines <$> readFile "input.txt"
+          putStrLn $ "Part 1: " ++ show (snd $ step grid)
+          putStrLn $ "Part 2: " ++ show (simulate grid)
