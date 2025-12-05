954f3c08f58af5ad590c5d20f5121f63aa35a200
Lorin-Lange <lo.-lange@arcor.de>
2025-12-05 19:15:10 +0100

    simplied solution of day 5 of AoC '25

--- AoC_2025/AoC_2025_05/Main.hs
+++ AoC_2025/AoC_2025_05/Main.hs
@@ -10,13 +10,10 @@ import Data.List.Split (splitOn)
 import Data.Ix (Ix(inRange))
 import Data.List (foldl', sortOn)
 
-parseDB :: String -> ([(Int, Int)], [Int])
-parseDB input = (parsedRanges, parsedIds)
-   where (rawRanges, _ : rawIds) = break null $ lines input
-         parse         = map $ toPair . splitOn "-"
-         toPair [s, e] = (read s, read e)
-         parsedIds     = map read rawIds
-         parsedRanges  = sortOn fst $ parse rawRanges
+parse :: String -> ([(Int, Int)], [Int])
+parse input = (sortOn fst $ toPairs ranges, map read ids)
+   where (ranges, _:ids) = break null $ lines input
+         toPairs = map $ (\[s, e] -> (read s, read e)) . splitOn "-"
 
 part1 :: [Int] -> [(Int, Int)] -> Int
 part1 ids ranges = sum [ 1 | id <- ids, any (`inRange` id) ranges ]
@@ -28,6 +25,6 @@ part2 = fst . foldl' go (0, 0)
                           | otherwise = (t + e - le, e)
 
 main :: IO()
-main = do (ranges, ids) <- parseDB <$> readFile "input.txt"
+main = do (ranges, ids) <- parse <$> readFile "input.txt"
           putStrLn $ "Part 1: " ++ show (part1 ids ranges)
           putStrLn $ "Part 2: " ++ show (part2 ranges)
