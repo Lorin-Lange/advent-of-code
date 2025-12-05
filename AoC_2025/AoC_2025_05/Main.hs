57d0c993ee92aad72f4e47b80c5d12df7a5a5122
Lorin-Lange <lo.-lange@arcor.de>
2025-12-05 21:04:44 +0100

    simplied solution of day 5 of AoC '25

--- AoC_2025/AoC_2025_05/Main.hs
+++ AoC_2025/AoC_2025_05/Main.hs
@@ -7,24 +7,24 @@
 module Main (main) where
 
 import Data.List.Split (splitOn)
-import Data.Ix (Ix(inRange))
+import Data.Ix (inRange)
 import Data.List (foldl', sortOn)
 
 parse :: String -> ([(Int, Int)], [Int])
-parse input = (sortOn fst $ map toPair ranges, map read ids)
-   where (ranges, _:ids) = break null $ lines input
-         toPair = (\[s, e] -> (read s, read e)) . splitOn "-"
+parse input = (sortOn fst $ map toPair rngs, map read ids)
+   where (rngs, _:ids) = break null $ lines input
+         toPair        = (\[s, e] -> (read s, read e)) . splitOn "-"
 
-part1 :: [Int] -> [(Int, Int)] -> Int
-part1 ids ranges = sum [ 1 | id <- ids, any (`inRange` id) ranges ]
+part1 :: [(Int, Int)] -> [Int] -> Int
+part1 rngs = length . filter (\id -> any (`inRange` id) rngs)
 
 part2 :: [(Int, Int)] -> Int
 part2 = fst . foldl' go (0, 0)
-  where go (t, le) (s, e) | le >= e   = (t, le)
-                          | le < s    = (t + e - s + 1, e)
-                          | otherwise = (t + e - le, e)
+   where go (t, le) (s, e) | le >= e   = (t, le)
+                           | le < s    = (t + e - s + 1, e)
+                           | otherwise = (t + e - le, e)
 
 main :: IO()
-main = do (ranges, ids) <- parse <$> readFile "input.txt"
-          putStrLn $ "Part 1: " ++ show (part1 ids ranges)
-          putStrLn $ "Part 2: " ++ show (part2 ranges)
+main = do (rngs, ids) <- parse <$> readFile "input.txt"
+          putStrLn $ "Part 1: " ++ show (part1 rngs ids)
+          putStrLn $ "Part 2: " ++ show (part2 rngs)
