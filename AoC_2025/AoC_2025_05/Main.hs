fc5090a9a679d06081624fef1ed5d6c78c6514f0
Lorin-Lange <lo.-lange@arcor.de>
2025-12-05 18:51:45 +0100

    simplied solution of day 5 of AoC '25

--- AoC_2025/AoC_2025_05/Main.hs
+++ AoC_2025/AoC_2025_05/Main.hs
@@ -4,38 +4,30 @@
 --            Solution by Lorin Lange             --
 ----------------------------------------------------
 
-{-# LANGUAGE OverloadedRecordDot   #-}
-{-# LANGUAGE DuplicateRecordFields #-}
-
 module Main (main) where
 
 import Data.List.Split (splitOn)
 import Data.Ix (Ix(inRange))
 import Data.List (foldl', sortOn)
 
-data DB = DB
-  { ranges :: [(Int, Int)]
-  , ids    :: [Int]
-  } deriving Show
-
-parseDB :: String -> DB
-parseDB input = DB { ranges = parsedRanges, ids = parsedIds }
+parseDB :: String -> ([(Int, Int)], [Int])
+parseDB input = (parsedRanges, parsedIds)
    where (rawRanges, _ : rawIds) = break null $ lines input
          parse         = map $ toPair . splitOn "-"
          toPair [s, e] = (read s, read e)
          parsedIds     = map read rawIds
          parsedRanges  = sortOn fst $ parse rawRanges
 
-part1 :: DB -> Int
-part1 db = sum [ 1 | id <- db.ids, any (`inRange` id) db.ranges ]
+part1 :: [Int] -> [(Int, Int)] -> Int
+part1 ids ranges = sum [ 1 | id <- ids, any (`inRange` id) ranges ]
 
-part2 :: DB -> Int
-part2 db = fst $ foldl' go (0, 0) db.ranges
+part2 :: [(Int, Int)] -> Int
+part2 ranges = fst $ foldl' go (0, 0) ranges
   where go (t, le) (s, e) | le >= e   = (t, le)
                           | le < s    = (t + e - s + 1, e)
                           | otherwise = (t + e - le, e)
 
 main :: IO()
-main = do db <- parseDB <$> readFile "input.txt"
-          putStrLn $ "Part 1: " ++ show (part1 db)
-          putStrLn $ "Part 2: " ++ show (part2 db)
+main = do (ranges, ids) <- parseDB <$> readFile "input.txt"
+          putStrLn $ "Part 1: " ++ show (part1 ids ranges)
+          putStrLn $ "Part 2: " ++ show (part2 ranges)
